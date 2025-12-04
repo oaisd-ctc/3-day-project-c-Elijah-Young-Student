321d491e3a142b0092876ce833db809adc6a0167
Landon Bolt <labo27b@student.oaisd.org>
2025-12-04 18:59:35 +0000

    Got most pieces made

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using pieces;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        pawn Pawn = new pawn();
+        pawn.pawnArt();
+        Thread.Sleep(2000);
+        Console.Clear();
+        pawn.rookPiece();
+        Thread.Sleep(2000);
+        Console.Clear();
+        pawn.knightPiece();
+        Thread.Sleep(2000);
+        Console.Clear();
+        pawn.bishopPiece();
+        Thread.Sleep(2000);
+        pawn.kingPiece();
+    }
+}
---
+++ pieces/pawn.cs
@@ -0,0 +1,78 @@
+namespace pieces;
+
+
+class pawn
+{
+    public static void pawnArt()
+    {
+        Console.Clear();
+        Console.WriteLine("        :#@*        ");
+        Console.WriteLine("       %@@@@@       ");
+        Console.WriteLine("       %@@@@@       ");
+        Console.WriteLine("       =@@@@#:      ");
+        Console.WriteLine("       =@@@@+       ");
+        Console.WriteLine("     -+*@@@@#@-     ");
+        Console.WriteLine("     %@@@@@@@@@     ");
+        Console.WriteLine("    -@@@@@@@@@@+    ");
+        
+    }
+    public static void rookPiece()
+    {
+        Console.WriteLine("     :. .:. .:      ");
+        Console.WriteLine("     @%#%@%#%@      ");
+        Console.WriteLine("     %@@@@@@@%      ");
+        Console.WriteLine("      -*###*-       ");
+        Console.WriteLine("      :@@@@@-       ");
+        Console.WriteLine("      :@@@@@-       ");
+        Console.WriteLine("      -@@@@@=       ");
+        Console.WriteLine("      *@@@@@*.      ");
+        Console.WriteLine("     ::::::::.      ");
+        Console.WriteLine("   :==--------=-    ");
+        Console.WriteLine("    .:::::::::.     ");
+    }
+    public static void knightPiece()
+    {
+        Console.WriteLine("          *#        ");
+        Console.WriteLine("        =@@@@#@     ");
+        Console.WriteLine("     .@@@@@@@@#@    ");
+        Console.WriteLine("     @@@@@@@@@@@#   ");
+        Console.WriteLine("          @@@@+@+   ");
+        Console.WriteLine("        +@@@@@@@    ");
+        Console.WriteLine("       @#@@@@@@@    ");
+        Console.WriteLine("      @#@@@@@@@@    ");
+        Console.WriteLine("       @@@@@@@#-    ");
+        Console.WriteLine("       @@@@@@@#     ");
+        Console.WriteLine("      @-@@@@@@@%    ");
+        Console.WriteLine("     -#*+====+*#    ");               
+    }
+    public static void bishopPiece()
+    {
+        Console.Clear();
+        Console.WriteLine("        :#@*        ");
+        Console.WriteLine("       %@@@@@       ");
+        Console.WriteLine("       %@@@@@       ");
+        Console.WriteLine("       =@@@@#:      ");
+        Console.WriteLine("       =@@@@+       ");
+        Console.WriteLine("        @@@@        ");
+        Console.WriteLine("        @@@@        ");
+        Console.WriteLine("       =@@@@+       ");
+        Console.WriteLine("     -+*@@@@#@-     ");
+        Console.WriteLine("     %@@@@@@@@@     ");
+        Console.WriteLine("    -@@@@@@@@@@+    ");
+    }
+    public static void kingPiece()
+    {
+        Console.Clear();
+        Console.WriteLine("         @@         ");
+        Console.WriteLine("      #--##--#      ");
+        Console.WriteLine("         @@         ");
+        Console.WriteLine("        :#@*        ");
+        Console.WriteLine("       %@@@@@       ");
+        Console.WriteLine("       %@@@@@       ");
+        Console.WriteLine("       =@@@@#:      ");
+        Console.WriteLine("       =@@@@+       ");
+        Console.WriteLine("     -+*@@@@#@-     ");
+        Console.WriteLine("     %@@@@@@@@@     ");
+        Console.WriteLine("    -@@@@@@@@@@+    ");
+    }
+}
