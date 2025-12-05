09cd06e07e739691653860b8fc0396be5f43f072
Landon Bolt <labo27b@student.oaisd.org>
2025-12-05 18:58:40 +0000

    Made game just no rules to movement yet

--- Program.cs
+++ Program.cs
@@ -5,17 +5,8 @@ class Program
     public static void Main(string[] args)
     {
         pawn Pawn = new pawn();
-        pawn.pawnArt();
-        Thread.Sleep(2000);
         Console.Clear();
-        pawn.rookPiece();
-        Thread.Sleep(2000);
-        Console.Clear();
-        pawn.knightPiece();
-        Thread.Sleep(2000);
-        Console.Clear();
-        pawn.bishopPiece();
-        Thread.Sleep(2000);
-        pawn.kingPiece();
+        pawn.run();
+
     }
 }
